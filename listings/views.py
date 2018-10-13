c36dc1b41ad92a6d53f77a9360bb2cd0ed727f27
nathaniel95975 <nathaniel95975@cccneb.edu>
2018-10-12 20:23:50 -0500

    Listing Page dynamic data

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import get_object_or_404, render
+
+from .models import Listing
 
 
 def index(request):
-  return render(request, 'listings/listings.html')
+  listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-def listing(request):
-  return render(request, 'listings/listing.html')
+  paginator = Paginator(listings, 6)
+  page = request.GET.get('page')
+  paged_listings = paginator.get_page(page)
+
+  contex = {
+    'listings': paged_listings
+  }
+
+  return render(request, 'listings/listings.html', contex)
+
+def listing(request, listing_id):
+  listing = get_object_or_404(Listing, pk=listing_id)
+
+  context = {
+    'listing': listing
+  }
+
+  return render(request, 'listings/listing.html', context)
 
 def search(request):
   return render(request, 'listings/search.html')
