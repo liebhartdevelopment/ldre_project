f05130e25532592264e2d2ffdee95920eee0ae9a
nathaniel95975 <nathaniel95975@cccneb.edu>
2018-10-15 20:03:01 -0500

    Email realtor & Inquiries on Dashboard

--- ldre/settings.py
+++ ldre/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'accounts.apps.AccountsConfig',
     'realtors.apps.RealtorsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -140,3 +141,10 @@ MEDIA_URL = '/media/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'nathaniel95975@gmail.com'
+EMAIL_HOST_PASSWORD = 'cSharpMain()'
+EMAIL_USE_TLS = True
