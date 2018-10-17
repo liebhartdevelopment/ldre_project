34d2fce5edcc734e3e48eee7513bbf745b78f093
liebhartdevelopment <44184616+liebhartdevelopment@users.noreply.github.com>
2018-10-16 19:54:42 -0500

    Update settings.py
    removed sensitive data

--- ldre/settings.py
+++ ldre/settings.py
@@ -84,9 +84,9 @@ WSGI_APPLICATION = 'ldre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ldredb',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
@@ -145,6 +145,6 @@ MESSAGE_TAGS = {
 # Email config
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'nathaniel95975@gmail.com'
-EMAIL_HOST_PASSWORD = 'cSharpMain()'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
